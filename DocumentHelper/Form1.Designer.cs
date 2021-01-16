8a673e6f32f14d8fd431d388cf33c4d12d5c863d
ssmc <mynasreenbanu@gmail.com>
2021-01-16 19:02:00 +0530

    added hotstrings tab

--- DocumentHelper/Form1.Designer.cs
+++ DocumentHelper/Form1.Designer.cs
@@ -40,9 +40,13 @@ namespace DocumentHelper
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -51,10 +55,11 @@ namespace DocumentHelper
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Font = new System.Drawing.Font("Tahoma", 14F);
-            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(0, 0);
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1088, 682);
+            this.tabControl1.Size = new System.Drawing.Size(1170, 654);
             this.tabControl1.TabIndex = 5;
             // 
             // tabPage1
@@ -64,16 +69,16 @@ namespace DocumentHelper
             this.tabPage1.Controls.Add(this.Lv);
             this.tabPage1.Location = new System.Drawing.Point(4, 32);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1080, 646);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(1162, 618);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Typing   ";
             // 
             // Rt
             // 
-            this.Rt.Location = new System.Drawing.Point(6, 6);
+            this.Rt.Location = new System.Drawing.Point(8, 8);
             this.Rt.Name = "Rt";
-            this.Rt.Size = new System.Drawing.Size(718, 634);
+            this.Rt.Size = new System.Drawing.Size(793, 603);
             this.Rt.TabIndex = 8;
             this.Rt.Text = "";
             // 
@@ -81,9 +86,9 @@ namespace DocumentHelper
             // 
             this.Lv.Font = new System.Drawing.Font("Tahoma", 10F);
             this.Lv.HideSelection = false;
-            this.Lv.Location = new System.Drawing.Point(730, 6);
+            this.Lv.Location = new System.Drawing.Point(807, 7);
             this.Lv.Name = "Lv";
-            this.Lv.Size = new System.Drawing.Size(341, 634);
+            this.Lv.Size = new System.Drawing.Size(349, 605);
             this.Lv.TabIndex = 7;
             this.Lv.UseCompatibleStateImageBehavior = false;
             // 
@@ -97,42 +102,45 @@ namespace DocumentHelper
             this.tabPage2.Controls.Add(this.TbWeb);
             this.tabPage2.Location = new System.Drawing.Point(4, 32);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1080, 646);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(1162, 618);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Database   ";
             // 
             // BtnAdd
             // 
             this.BtnAdd.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.BtnAdd.Location = new System.Drawing.Point(652, 57);
+            this.BtnAdd.Location = new System.Drawing.Point(525, 550);
             this.BtnAdd.Name = "BtnAdd";
-            this.BtnAdd.Size = new System.Drawing.Size(128, 33);
+            this.BtnAdd.Size = new System.Drawing.Size(149, 40);
             this.BtnAdd.TabIndex = 5;
             this.BtnAdd.Text = "Add To Db";
             this.BtnAdd.UseVisualStyleBackColor = true;
             // 
             // TbWeb
             // 
-            this.TbWeb.Location = new System.Drawing.Point(240, 57);
+            this.TbWeb.Location = new System.Drawing.Point(280, 83);
             this.TbWeb.Name = "TbWeb";
-            this.TbWeb.Size = new System.Drawing.Size(339, 30);
+            this.TbWeb.Size = new System.Drawing.Size(394, 30);
             this.TbWeb.TabIndex = 4;
             // 
             // tabPage3
             // 
+            this.tabPage3.BackColor = System.Drawing.Color.Silver;
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.textBox2);
             this.tabPage3.Location = new System.Drawing.Point(4, 32);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1080, 646);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage3.Size = new System.Drawing.Size(1162, 618);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "HotStrings   ";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 64);
+            this.label1.Location = new System.Drawing.Point(26, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(197, 23);
             this.label1.TabIndex = 6;
@@ -142,34 +150,67 @@ namespace DocumentHelper
             // 
             this.textBox1.AcceptsReturn = true;
             this.textBox1.AcceptsTab = true;
-            this.textBox1.Location = new System.Drawing.Point(240, 151);
+            this.textBox1.Location = new System.Drawing.Point(280, 144);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(339, 374);
+            this.textBox1.Size = new System.Drawing.Size(394, 370);
             this.textBox1.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 151);
+            this.label2.Location = new System.Drawing.Point(26, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(156, 23);
             this.label2.TabIndex = 8;
             this.label2.Text = "Text to add to db";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Add hotStrings";
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 18F);
+            this.textBox2.Location = new System.Drawing.Point(205, 34);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(391, 515);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label4.Location = new System.Drawing.Point(602, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 189);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Examples:\r\n\r\nggc - google cloud\r\nprt   - please read this\r\nhau   - how are you?\r\n" +
+    "\r\n\r\n(put each in a new line, \r\nwith hypen - as delimiter)";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(1093, 691);
+            this.ClientSize = new System.Drawing.Size(1174, 660);
             this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Tahoma", 10F);
             this.Name = "Form1";
             this.Text = "Document Typing Helper";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -187,6 +228,9 @@ namespace DocumentHelper
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
