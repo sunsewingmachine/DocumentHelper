a451d932864c38ca39ee2ac1cbe152bd6752c66b
ssmc <mynasreenbanu@gmail.com>
2021-01-16 18:15:20 +0530

    added tabs

--- DocumentHelper/Form1.Designer.cs
+++ DocumentHelper/Form1.Designer.cs
@@ -29,73 +29,164 @@ namespace DocumentHelper
         /// </summary>
         private void InitializeComponent()
         {
-            this.Tb = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Rt = new System.Windows.Forms.RichTextBox();
             this.Lv = new System.Windows.Forms.ListView();
-            this.TbWeb = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.BtnAdd = new System.Windows.Forms.Button();
+            this.TbWeb = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Tb
+            // tabControl1
             // 
-            this.Tb.AcceptsReturn = true;
-            this.Tb.AcceptsTab = true;
-            this.Tb.Font = new System.Drawing.Font("Tahoma", 22F);
-            this.Tb.HideSelection = false;
-            this.Tb.Location = new System.Drawing.Point(12, 12);
-            this.Tb.Multiline = true;
-            this.Tb.Name = "Tb";
-            this.Tb.Size = new System.Drawing.Size(666, 528);
-            this.Tb.TabIndex = 0;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1088, 682);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Silver;
+            this.tabPage1.Controls.Add(this.Rt);
+            this.tabPage1.Controls.Add(this.Lv);
+            this.tabPage1.Location = new System.Drawing.Point(4, 32);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1080, 646);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Typing   ";
+            // 
+            // Rt
+            // 
+            this.Rt.Location = new System.Drawing.Point(6, 6);
+            this.Rt.Name = "Rt";
+            this.Rt.Size = new System.Drawing.Size(718, 634);
+            this.Rt.TabIndex = 8;
+            this.Rt.Text = "";
             // 
             // Lv
             // 
             this.Lv.Font = new System.Drawing.Font("Tahoma", 10F);
             this.Lv.HideSelection = false;
-            this.Lv.Location = new System.Drawing.Point(684, 12);
+            this.Lv.Location = new System.Drawing.Point(730, 6);
             this.Lv.Name = "Lv";
-            this.Lv.Size = new System.Drawing.Size(341, 387);
-            this.Lv.TabIndex = 1;
+            this.Lv.Size = new System.Drawing.Size(341, 634);
+            this.Lv.TabIndex = 7;
             this.Lv.UseCompatibleStateImageBehavior = false;
             // 
-            // TbWeb
+            // tabPage2
             // 
-            this.TbWeb.Location = new System.Drawing.Point(686, 481);
-            this.TbWeb.Name = "TbWeb";
-            this.TbWeb.Size = new System.Drawing.Size(339, 20);
-            this.TbWeb.TabIndex = 2;
+            this.tabPage2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.BtnAdd);
+            this.tabPage2.Controls.Add(this.TbWeb);
+            this.tabPage2.Location = new System.Drawing.Point(4, 32);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1080, 646);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Database   ";
             // 
             // BtnAdd
             // 
             this.BtnAdd.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.BtnAdd.Location = new System.Drawing.Point(895, 507);
+            this.BtnAdd.Location = new System.Drawing.Point(652, 57);
             this.BtnAdd.Name = "BtnAdd";
             this.BtnAdd.Size = new System.Drawing.Size(128, 33);
-            this.BtnAdd.TabIndex = 3;
+            this.BtnAdd.TabIndex = 5;
             this.BtnAdd.Text = "Add To Db";
             this.BtnAdd.UseVisualStyleBackColor = true;
+            // 
+            // TbWeb
+            // 
+            this.TbWeb.Location = new System.Drawing.Point(240, 57);
+            this.TbWeb.Name = "TbWeb";
+            this.TbWeb.Size = new System.Drawing.Size(339, 30);
+            this.TbWeb.TabIndex = 4;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 32);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1080, 646);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "HotStrings   ";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Website/Webpage link";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(240, 151);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(339, 374);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Text to add to db";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1035, 551);
-            this.Controls.Add(this.BtnAdd);
-            this.Controls.Add(this.TbWeb);
-            this.Controls.Add(this.Lv);
-            this.Controls.Add(this.Tb);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1093, 691);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Document Typing Helper";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox Tb;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox Rt;
         private System.Windows.Forms.ListView Lv;
-        private System.Windows.Forms.TextBox TbWeb;
+        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.TextBox TbWeb;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
